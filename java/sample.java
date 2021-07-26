3d132ce4916a8dbd8f2db3afa617616231d294ba
hikaru <skyvader0524@gmail.com>
2021-07-26 13:28:42 +0900

    Add files via upload

---
+++ java/sample.java
@@ -0,0 +1,16 @@
+class Hello {
+public static void main(String[] args){
+  System.out.println("Hello");
+}
+}
+
+// ヌルプログラム
+// mainメソッドと呼ばれ、 mainの書かれた場所からjavaのプログラムは実行される
+
+class Hello {
+public static void main(String[] args){
+  // ２行書きたい時
+  System.out.println("Hello");
+  System.out.println("Good");
+}
+}
