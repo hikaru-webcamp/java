6deda7121aa834a49bf0f13cedc86fbbd6701b1c
hikaru <skyvader0524@gmail.com>
2021-08-11 18:39:52 +0900

    Add files via upload

---
+++ java3.java
@@ -0,0 +1,10 @@
+class Enzanshi1{
+  public static void main(String[] args){
+    System.out.println(123 + 456);
+    System.out.println((1+2)*5);
+    System.out.print("1 / 3.0 = ");
+    System.out.println(1 / 3.0 );
+    System.out.println("Good" + "morning" + ".");  // 文字列の結合
+
+  }
+}
