8ea12942babe8dbd6e227ab321bba87eedc22d78
hikaru <skyvader0524@gmail.com>
2021-08-12 20:57:06 +0900

    Add files via upload

---
+++ java4.java
@@ -0,0 +1,16 @@
+class Enzanshi1{
+  public static void main(String[] args){
+    System.out.println(123 + 456);
+    System.out.println((1+2)*5);
+    System.out.print("1 / 3.0 = ");
+    System.out.println(1 / 3.0 );
+    System.out.println("Good" + "morning" + ".");  // 文字列の結合
+    System.out.println("123 + 456 = " + (123+456));
+    System.out.println("A + 4 = " + 'A' + 1);
+    System.out.println( "'A' + 1 = " + ('A' + 1) ); // 文字と数値の足し算は文字コードと+される ()があると違う出力
+
+    int x;
+    x = 1 + 2;
+    System.out.println(x);
+  }
+}
