b879054378a1c91f2f4dfa1f8ac21a0ddb436807
hikaru <skyvader0524@gmail.com>
2021-08-14 12:12:35 +0900

    文字型変数の学習
    文字の型 char
    文字列の型 String
    
    lengthメソッドで文字列の長さを表示できる

---
+++ java9.java
@@ -0,0 +1,45 @@
+// 文字型の変数
+
+// class Hensu_char {
+//   public static void main(String[] args){
+//     char moji_H, moji_E, moji_L, moji_O;
+
+//     moji_H = 'H';
+//     moji_E = 'E';
+//     moji_L = 'L';
+//     moji_O = 'O';
+
+//     System.out.print(moji_H);
+//     System.out.print(moji_E);
+//     System.out.print(moji_L);
+//     System.out.print(moji_L);
+//     System.out.println(moji_O);
+
+//   }
+// }
+
+// 文字列の変数型
+// class Obj_string1 {
+//   public static void main(String[] args){
+//     String message1 = "Good ";
+//     String message2 = "morning ";
+//     String message;
+
+//     message = message1 + message2;
+//     System.out.println(message);
+
+//   }
+// }
+
+// 文字列の長さを調べる
+class Obj_string1 {
+  public static void main(String[] args){
+    String message1 = "Good ";
+    String message2 = "morning ";
+    String message;
+
+    message = message1 + message2;
+    System.out.println(message.length());
+
+  }
+}
