cf906032d5afd0a6949b8e332dd5a2771145e85f
hikaru <skyvader0524@gmail.com>
2021-08-14 12:32:08 +0900

    論理型変数と型の推論
    varを使って変数宣言すると、型はコンパイラによって決定される。

---
+++ java10.java
@@ -0,0 +1,27 @@
+// class Hensu_boolean{
+//   public static void main(String[] args){
+//     boolean yes, no;
+
+//     yes = true;
+//     no = false;
+
+//     System.out.println("yes = " + yes);
+//     System.out.println("no = " + no);
+
+//   }
+
+// }
+
+
+// varを使えば、型はコンパイラによって決定される
+class Siron{
+  public static void main(String[] args){
+    var x = 123;
+    var y = 4.56;
+    var z = "HEllo";
+
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+  }
+}
