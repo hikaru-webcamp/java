caa91268e73a8c649dc670bf1abe28ce1d1c7eb9
hikaru <skyvader0524@gmail.com>
2021-08-14 12:11:04 +0900

    実数型変数の学習
    floatとdouble

---
+++ java8.java
@@ -0,0 +1,24 @@
+// class Hensyu_double{
+//   public static void main(String [] args){
+//     double shincho, taiju;
+
+//     shincho = 1.75;
+//     taiju = 22 * shincho * shincho;
+//     System.out.println("身長" + shincho + "mの人の標準体重は");
+//     System.out.println(taiju + "kgです。");
+//   }
+// }
+
+class Hensu_float1{
+  public static void main(String[] args){
+    int shincho;
+    float taiju;
+
+    shincho = 175;
+    taiju =(shincho - 100) * 0.9f;
+    System.out.println("身長" + shincho + "mの人の標準体重は");
+    System.out.println(taiju + "kgです。");
+
+
+  }
+}
